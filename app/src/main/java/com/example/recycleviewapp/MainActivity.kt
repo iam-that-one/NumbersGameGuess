16b507c982921f4987651b18666c4d84613e0e2a
iam-that-one <iam-that-one@users.noreply.github.com>
2021-09-27 14:00:24 +0300

    nitilize

--- app/src/main/java/com/example/recycleviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recycleviewapp/MainActivity.kt
@@ -9,27 +9,61 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     lateinit var add : Button
     lateinit var txt : EditText
     lateinit var lbl : TextView
-    var students = mutableListOf<String>()
+    lateinit var  lbl1 : TextView
+    lateinit var clear : Button
+    var message = ""
+    lateinit var students : ArrayList<String>
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         txt = findViewById(R.id.editTextTextPersonName3)
+        students = ArrayList()
         add = findViewById(R.id.button2)
+        lbl1 = findViewById(R.id.textView)
+        clear = findViewById(R.id.button1)
+        clear.alpha = 0F
       //  lbl = findViewById(R.id.textView6)
 
         val myRV = findViewById<RecyclerView>(R.id.rvMain)
         myRV.adapter = RecycleViewAddapter(students)
         myRV.layoutManager = LinearLayoutManager(this)
+        var ans = ""
+        var rand = 0
 
+        lbl1.text = "Guess the correct number [***]"
+        clear.setOnClickListener{
+            lbl1.text = "Guess the correct number [***]"
+          //  students =  mutableListOf("")
+            students.add("----------------------------------------------------")
+            clear.alpha = 0F
+            students.clear()
+            myRV.adapter?.notifyDataSetChanged()
+        }
         add.setOnClickListener {
-            students.add(txt.text.toString())
-          //  lbl.text = students.toString()
+             ans = txt.text.toString()
+             rand = Random.nextInt(101)
+            if(ans == rand.toString()){
+                lbl1.text = "Guess the correct number [Unlocked] [${ans}]"
+                students.add("Your guss is ${ans}, You got it!! 😊")
+
+                clear.alpha = 1F
+
+            }else{
+
+                students.add("Sorry!! Wrong guess 😐")
+
+            }
+            myRV.adapter?.notifyDataSetChanged()
+            txt.text.clear()
+
         }
         println(students)
     }
--- app/src/main/java/com/example/recycleviewapp/RecycleViewAddapter.kt
+++ app/src/main/java/com/example/recycleviewapp/RecycleViewAddapter.kt
@@ -6,7 +6,7 @@ import android.telephony.ims.RcsUceAdapter
 import android.view.LayoutInflater
 import kotlinx.android.synthetic.main.item_row.view.*
 
-class RecycleViewAddapter(val students : List<String>) : RecyclerView.Adapter<RecycleViewAddapter.ItemViewHolder>() {
+class RecycleViewAddapter(val students : ArrayList<String>) : RecyclerView.Adapter<RecycleViewAddapter.ItemViewHolder>() {
     class ItemViewHolder(itemView: View)  : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -23,10 +23,10 @@ class RecycleViewAddapter(val students : List<String>) : RecyclerView.Adapter<Re
       val student = students[position]
         holder.itemView.apply {
             textView6.text = student
+
         }
     }
 
     override fun getItemCount() = students.size
-
 
 }
